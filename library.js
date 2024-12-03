ef211c8901a20a8f6c2089fe2c4f10a86104b9b9
c-doo95 <116585057+c-doo95@users.noreply.github.com>
2024-12-03 16:03:39 -0500

    create addToLibrary function

--- library.js
+++ library.js
@@ -7,3 +7,10 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+}
+
+const redMars = new Book('Red Mars', 'Kim Stanley Robinson', 519, true);
+
+const lEtranger = new Book('The Stranger', 'Albert Camus', 159, true);
